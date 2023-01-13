1547d33bf9b9337436c046ad075901857a241ae9
Riley McCullough <rileybmccullough@gmail.com>
2023-01-13 12:46:44 -0700

    Quicksave

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -3,8 +3,8 @@ class Api::V1::CustomersController < ApplicationController
   def show
     user = Customer.find(params[:id])
     info = user.subscriptions
-
     render json: {user: user, data: info}
+
   end
 
 end
--- spec/requests/v1/customers/show_spec.rb
+++ spec/requests/v1/customers/show_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe 'customer#show' do
 
       result = JSON.parse(response.body, symbolize_names: true)
       expect(result).to have_key(:data)
-      # require "pry"; binding.pry
+
 
       data = result[:data]
       expect(data.count).to eq(1)
@@ -33,4 +33,21 @@ RSpec.describe 'customer#show' do
     end
 
   end
+
+  # it 'returns a json response with information about customers subscriptions' do
+  #
+  #   @tea101 = Tea.create(title: "Earl Grey", description: "Black tea", temperature: "100 degrees f", brew_time: "5 min" )
+  #   @tea102 = Tea.create(title: "Jasmin", description: "Herbal tea", temperature: "90 degrees f", brew_time: "5 min" )
+  #   @tea103 = Tea.create(title: "Irish Breakfast", description: "Black tea", temperature: "103 degrees f", brew_time: "4 min" )
+  #   @user101 = Customer.create(first_name: "Henry", last_name: "Spencer", email: "HS@gmail", address: "1234 John St")
+  #   @user102 = Customer.create(first_name: "Shawn", last_name: "Spencer", email: "ShawnS@gmail", address: "1444 E 12th St")
+  #
+  #   @subscription101 = Subscription.create(title: "Breakfast Basket", price: 48.99, status: true , frequency: "Every 2 weeks", customer_id: @user101.id, tea_id: @tea101.id )
+  #
+  #   get "/api/v1/customers/9999"
+  #
+  #   expect(response).to_not be_successful
+  #   expect(response).to have_http_status(204)
+  # end
+
 end
